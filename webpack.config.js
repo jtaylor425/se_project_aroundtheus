1d6727870f64837c90e6d5a0a26388e653ca2f71
joeltaylor425 <joeltaylor425@gmail.com>
2024-11-14 09:35:00 -0500

    commit changes to file paths

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   devtool: "inline-source-map",
   entry: {
-    main: "./src/pages/index.js",
+    main: "/src/pages/index.js",
   },
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -20,7 +20,7 @@ module.exports = {
   devServer: {
     static: path.resolve(__dirname, "dist"),
     compress: true,
-    port: 5507,
+    port: 5509,
     open: true,
     liveReload: true,
     hot: false,
@@ -50,7 +50,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: "/src/index.html",
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
