5f781647a7bd3c597ae5425228cd1e73a0111a78
joeltaylor425 <joeltaylor425@gmail.com>
2024-11-19 21:22:34 -0500

    remove curly braces

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
   devServer: {
     static: path.resolve(__dirname, "dist"),
     compress: true,
-    port: 5514,
+    port: 5515,
     open: true,
     liveReload: true,
     hot: false,
